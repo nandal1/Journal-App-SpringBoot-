3d52152b5963f0b8409e87734b509f7b958050be
adarshnandal <adarshnandal3@gmail.com>
2025-03-07 14:12:26 -0500

    up

--- src/main/java/net/engineeringdigest/journalApp/Controller/PublicController.java
+++ src/main/java/net/engineeringdigest/journalApp/Controller/PublicController.java
@@ -34,7 +34,7 @@ public class PublicController {
     @GetMapping("/health-check")
     public String healthCheck() {
         log.info("Health is ok !");
-        return "Ok";
+        return "Ok Adarsh!!";
     }
 
     @GetMapping
